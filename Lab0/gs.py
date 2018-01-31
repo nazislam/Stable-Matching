e9fa5a4056995c0b3bb0d569eafa761befa5ccce
Naz Islam <naz@nazislam.com>
2018-01-31 17:27:25 -0500

    Random-shuffle function and Participant

---
+++ Lab0/gs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+#---> SUITORS AND GIRLS
+suitors = [
+            'Abe', 'Bob', 'Col', 'Dan', 'Ed', 
+            'Fred', 'Gav', 'Hal', 'Ian', 'Jon'
+          ]
+
+girls = [
+            'Abi', 'Bea', 'Cath', 'Dee', 'Eve',
+            'Fay', 'Gay', 'Hope', 'Ivy', 'Jan'
+        ]
+
+
+#---> PARTICIPANTS LIST
+print('Participants:')
+for suitor in suitors:
+    print(suitor, end="  ")
+print()
+for girl in girls:
+    print(girl, end="  ")
+print()
+print()
+
+
+print('Preferences:')
+
+
+
+
+
+
+
