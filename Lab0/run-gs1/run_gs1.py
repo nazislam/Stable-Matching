9a026cfbd2175f7b43abc8b35221c4a295803479
NazAl Islam <naz@subtlecoding.com>
2018-02-21 02:07:48 -0500

    Generated necessary files out of run_gs1.py

--- Lab0/run-gs1/run_gs1.py
+++ Lab0/run-gs1/run_gs1.py
@@ -3,9 +3,10 @@
 import gs1
 import sys
 import random
+from subprocess import call
 
 def generate_random_number():
-    return random.randint(1, 55000)
+    return random.randint(1, 5500)
 
 def main():
 
@@ -13,6 +14,7 @@ def main():
         trial_input = generate_random_number()
         gs1.main(trial_input)
     
+    call(["/usr/bin/gnuplot", "-persist", "model.gpt"])
 
 if __name__ == '__main__':
     main()
