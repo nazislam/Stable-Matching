2206337e25142bc1e50888e1697d05981ce80fd0
NazAl Islam <naz@subtlecoding.com>
2018-02-21 00:56:32 -0500

    Changed loop number

--- Lab0/run-gs1/run_gs1.py
+++ Lab0/run-gs1/run_gs1.py
@@ -5,11 +5,11 @@ import sys
 import random
 
 def generate_random_number():
-    return random.randint(1, 80)
+    return random.randint(1, 55000)
 
 def main():
 
-    for i in range(0, 3): 
+    for i in range(0, 20): 
         trial_input = generate_random_number()
         gs1.main(trial_input)
     
