fb895fa2edede602fbaca564e10524ab081fd5fb
Naz Islam <naz@nazislam.com>
2018-02-20 20:51:48 -0500

    Finished gs1.py! DONE

--- Lab0/part_02/gs_04.py
+++ Lab0/part_02/gs_04.py
@@ -33,13 +33,11 @@ def begin_matching(man, suitors, suitors_pref, girls_pref, free_men, tentative_e
     index = suitors.index(man)
     for woman in suitors_pref[index]:
 
-#         print('{} proposes to {}'.format(man, woman))
         taken_match = [couple for couple in tentative_engagements if woman in couple]
         
         if (len(taken_match) == 0):
             tentative_engagements.append([man, woman])
             free_men.remove(man)
-#             print('{} is engaged to {}'.format(man, woman))
             break
         elif (len(taken_match) > 0):
 
@@ -47,7 +45,6 @@ def begin_matching(man, suitors, suitors_pref, girls_pref, free_men, tentative_e
             potential_guy = girls_pref[girls.index(woman)].index(man)
 
             if (current_guy > potential_guy):
-#                 print('{} dumps {}'.format(woman, man))
 
                 # the new guy is engaged
                 free_men.remove(man)
@@ -75,64 +72,44 @@ def generate_random_time():
     return random.randint(1, 10)
 
 def main():
-    while True:
-        start_time = time.time()
-        start_clock = time.clock()
-        time_to_sleep = generate_random_time()
-        time.sleep(time_to_sleep)
+    start_time = time.time()
+    start_clock = time.clock()
+    time_to_sleep = generate_random_time()
+    time.sleep(time_to_sleep)
 
-        length = int(sys.argv[1])
+    length = int(sys.argv[1])
 
-        suitors = list(range(length)) # 20 (0 - 19)
+    suitors = list(range(length)) # 20 (0 - 19)
+    girls = list(range(len(suitors), 2*(len(suitors))))
+
+
+    suitors_pref = []
+    girls_pref = []
+
+    free_men = []
+
+
+    for i in range(0, len(suitors)):
+        random.shuffle(girls)
+        suitors_pref.append(girls)
         girls = list(range(len(suitors), 2*(len(suitors))))
 
+    for i in range(0, len(girls)):
+        random.shuffle(suitors)
+        girls_pref.append(suitors)
+        suitors = list(range(length)) # 20 (0 - 19)
+    
 
-#         print('\nParticipants:')
-#         print_participants(suitors, girls)
-
-        suitors_pref = []
-        girls_pref = []
-
-        free_men = []
-
-
-        for i in range(0, len(suitors)):
-            random.shuffle(girls)
-            suitors_pref.append(girls)
-            girls = list(range(len(suitors), 2*(len(suitors))))
-
-        for i in range(0, len(girls)):
-            random.shuffle(suitors)
-            girls_pref.append(suitors)
-            suitors = list(range(length)) # 20 (0 - 19)
-        
-
-#         print('\nPreferences:')
-#         print_suitors_preferences(suitors, suitors_pref)
-#         print_girls_preferences(girls, girls_pref)
-
-        tentative_engagements = []
-        init_free_men(suitors, free_men)
-        stable_matching(free_men, suitors, suitors_pref, girls_pref, tentative_engagements, girls)
+    tentative_engagements = []
+    init_free_men(suitors, free_men)
+    stable_matching(free_men, suitors, suitors_pref, girls_pref, tentative_engagements, girls)
 
 
-#         print('\nPairing:')
-#         print_pairings(tentative_engagements)
-
-        end_time = time.time()
-        end_clock = time.clock()
-        taken_time = round(end_time - start_time, 6)
-        taken_clock = round(end_clock - start_clock, 6)
-        print('Elapsed wall clock time:\t {}s'.format(taken_time))
-        print('Elapsed CPU time:\t\t {}s'.format(taken_clock))
-#         print('Stable matchup')
-
-        k = input("\nAnother trial? (y)es, (n)o\n").lower()
-        if (k == 'n' or k == 'no'):
-            break
-        else:
-            continue
-
+    end_time = time.time()
+    end_clock = time.clock()
+    taken_time = round(end_time - start_time, 6)
+    taken_clock = round(end_clock - start_clock, 6)
+    print('{}\t{}s'.format(length, taken_clock))
 
 
 
